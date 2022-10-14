2fe7d9410157dce98cb6d1fde622fbc28cdbaf5f
Malavika S <malavika@kissflow.com>
2022-10-14 17:37:53 +0530

    Changes

---
+++ Slack-Age-Bot/file_bot.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4207896240343-4219872393925-OFohabBc3nwuYfpPb0z3hIdC")
+	os.Setenv("CHANNEL_ID", "C046JDBC3S6")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"ZIPL.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
